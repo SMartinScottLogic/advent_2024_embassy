3f7d772ff33eb134549bed5eb3dcd7fa436ed54b
Simon Martin <smartin@scottlogic.com>
2024-12-31 04:04:30 +0000

    Aoc day5 (#11)
    * Day 5 migrated
    
    ---------
    
    Co-authored-by: Simon Martin <havvoric@gmail.com>

--- utils/src/collections/fixedvec.rs
+++ utils/src/collections/fixedvec.rs
@@ -17,6 +17,15 @@ impl<T: Default, const C: usize> Default for FixedVec<T, C> {
         Self::new()
     }
 }
+impl<T: Clone, const C: usize> Clone for FixedVec<T, C> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+            write_pos: self.write_pos.clone(),
+        }
+    }
+}
+
 impl<T: Default, const C: usize> FixedVec<T, C> {
     pub fn new() -> Self {
         Self {
@@ -52,6 +61,9 @@ impl<T, const C: usize> FixedVec<T, C> {
     }
     pub fn as_ref(&self) -> &[T] {
         &self.inner[..self.write_pos]
+    }
+    pub fn as_mut_ref(&mut self) -> &mut [T] {
+        &mut self.inner[..self.write_pos]
     }
     pub fn push(&mut self, element: T) {
         assert!(self.write_pos < C);
