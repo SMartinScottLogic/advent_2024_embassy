506d227eabe244e214784763e5ed29105ba913ab
Simon Martin <smartin@scottlogic.com>
2025-05-04 14:48:40 +0100

    Day 9 complete
    * Day 9 runs locally
    
    * Day 9 runs to completion

--- src/aoc/day9.rs
+++ src/aoc/day9.rs
@@ -1,37 +1,47 @@
 use arrayvec::ArrayVec;
+use static_cell::StaticCell;
 
-use crate::info;
+use nonmax::NonMaxU16;
+
+use crate::{debug, info};
 
 type ResultType = u64;
 
+const NUM_BLOCKS: usize = 95000;
+
 const FULL: &[u8] = include_bytes!("../../input/day9.full");
 const SAMPLE: &[u8] = include_bytes!("../../input/day9.sample");
+static BLOCKS: StaticCell<ArrayVec<Block, NUM_BLOCKS>> = StaticCell::new();
 
-pub struct Solution {}
+pub struct Solution {
+    blocks: &'static mut ArrayVec<Block, NUM_BLOCKS>,
+}
 impl super::utils::Solution for Solution {
     fn new() -> impl super::utils::Solution {
-        Self {}
+        let blocks = BLOCKS.init_with(|| ArrayVec::new());
+        Self { blocks }
     }
 
     fn run_sample(&mut self) {
-        run("sample", SAMPLE)
+        run("sample", SAMPLE, self.blocks)
     }
 
     fn run_full(&mut self) {
-        run("full", FULL)
+        run("full", FULL, self.blocks)
     }
 }
 
-fn run(label: &'static str, data: &[u8]) {
+fn run(label: &'static str, data: &[u8], blocks: &mut ArrayVec<Block, NUM_BLOCKS>) {
     info!("{} start processing", label);
-    let step1_answer = analyse_part1(data);
+    let step1_answer = analyse_part1(data, blocks);
+    info!("{} blocks count: {}", label, blocks.len());
     info!("{} part1 answer: {}", label, step1_answer);
-    let step2_answer = analyse_part2(data);
+    let step2_answer = analyse_part2(data, blocks);
     info!("{} part2 answer: {}", label, step2_answer);
 }
 
-fn analyse_part1(data: &[u8]) -> ResultType {
-    let mut blocks = diskmap(data);
+fn analyse_part1(data: &[u8], blocks: &mut ArrayVec<Block, NUM_BLOCKS>) -> ResultType {
+    diskmap(data, blocks);
     let mut s = 0;
     let mut e = blocks.len() - 1;
 
@@ -52,9 +62,74 @@ fn analyse_part1(data: &[u8]) -> ResultType {
     checksum(blocks.as_ref())
 }
 
-fn analyse_part2(_data: &[u8]) -> ResultType {
-    // TODO
-    0
+fn analyse_part2(data: &[u8], blocks: &mut ArrayVec<Block, NUM_BLOCKS>) -> ResultType {
+    diskmap(data, blocks);
+
+    let mut e = (blocks.len() - 1) as isize;
+    let mut seen = u16::MAX;
+
+    loop {
+        while e >= 0
+            && (
+                match blocks[e as usize] {
+                    Block::FileBlock(b) if b.get() < seen => false,
+                    _ => true,
+                }
+                // || handled.contains(&blocks[e as usize])
+            )
+        {
+            e -= 1;
+            debug!("e: {}, end: {}", e, (e >= 0));
+        }
+        if e < 0 {
+            break;
+        }
+        //handled.insert(blocks[e as usize]);
+        match blocks[e as usize] {
+            Block::FileBlock(b) => {
+                seen = b.get();
+            }
+            _ => panic!(),
+        };
+        let mut num_e = 0;
+        while (e - num_e) >= 0 && blocks[e as usize] == blocks[(e - num_e) as usize] {
+            num_e += 1;
+        }
+        debug!("block: {:?}, num_e: {}", blocks[e as usize], num_e);
+        let mut s = 0;
+        let (s, num_s, can_move) = loop {
+            while s < (e - num_e) && blocks[s as usize] != Block::Empty {
+                s += 1;
+            }
+            if s >= (e - num_e) || blocks[s as usize] != Block::Empty {
+                break (s, 0, false);
+            }
+            let mut num_s = 1;
+            while num_s < num_e
+                && ((s + num_s) as usize) < blocks.len()
+                && blocks[(s + num_s) as usize] == Block::Empty
+            {
+                num_s += 1;
+            }
+            if num_s >= num_e {
+                break (s, num_s, true);
+            }
+            s += num_s;
+        };
+        debug!(
+            "s: {}, num_s: {}, e: {}, can_move: {}",
+            s, num_s, e, can_move
+        );
+        if can_move {
+            for i in 0..num_e {
+                blocks[(s + i) as usize] = blocks[(e - num_e + 1 + i) as usize];
+                blocks[(e - num_e + 1 + i) as usize] = Block::Empty;
+            }
+        }
+    }
+    debug!("blocks: {:?}", blocks);
+
+    checksum(blocks.as_ref())
 }
 
 fn checksum(blocks: &[Block]) -> ResultType {
@@ -63,7 +138,7 @@ fn checksum(blocks: &[Block]) -> ResultType {
         .enumerate()
         .map(|(pos, block)| {
             if let Block::FileBlock(id) = block {
-                (*id as ResultType) * (pos as ResultType)
+                (id.get() as ResultType) * (pos as ResultType)
             } else {
                 0
             }
@@ -71,19 +146,20 @@ fn checksum(blocks: &[Block]) -> ResultType {
         .sum()
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 enum Block {
     #[default]
     Empty,
-    FileBlock(usize),
+    FileBlock(NonMaxU16),
 }
 
-fn diskmap(map: &[u8]) -> ArrayVec<Block, 200000> {
-    let mut blocks = ArrayVec::new();
+fn diskmap(map: &[u8], blocks: &mut ArrayVec<Block, NUM_BLOCKS>) {
+    blocks.clear();
     for (id, c) in map.iter().enumerate() {
         let c = *c - b'0';
         let s = if id % 2 == 0 {
-            Block::FileBlock(id / 2)
+            let id = NonMaxU16::new((id / 2) as u16).unwrap();
+            Block::FileBlock(id)
         } else {
             Block::Empty
         };
@@ -91,5 +167,4 @@ fn diskmap(map: &[u8]) -> ArrayVec<Block, 200000> {
             blocks.push(s);
         }
     }
-    blocks
 }
